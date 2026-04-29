c1aaa1726d5089619f491f5aa909b03fae74811b
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-04-29 15:28:59 +0500

    Remove avatar from CustomUserAdmin

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import FileResponse, Http404
+import os
+
+def serve_spa(request, path=''):
+    index_file = settings.WHITENOISE_ROOT / 'index.html'
+    if os.path.exists(index_file):
+        return FileResponse(open(index_file, 'rb'), content_type='text/html')
+    raise Http404('Frontend not built. Run npm run build.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +17,5 @@ urlpatterns = [
     path('api/', include('groups.urls')),
     path('api/', include('quiz.urls')),
     path('api/', include('tournament.urls')),
+    re_path(r'^(?!api/|admin/|static/|media/).*$', serve_spa),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
